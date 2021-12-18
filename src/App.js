87ed9836bfd2273f43a3ab19b8e702622bd34a21
celio <celio.ldn@gmail.com>
2021-12-18 15:58:38 -0300

    Branch title with master

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://netsono.com.br"
+          href="https://www.netsono.com.br"
           target="_blank"
           rel="noopener noreferrer"
         >
