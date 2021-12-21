83ba323df826fca59e99b29f83485b9bdce152dd
celio <celio.ldn@gmail.com>
2021-12-21 20:04:23 -0300

    Ajusted Front-End

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import FormRegister from "./components/form/form";
 import { Container, Typography } from "@material-ui/core";
+import "@fontsource/roboto/300.css";
 
 function App() {
   return (
     <Container component={"article"} maxWidth="sm">
-      <Typography variant="h1" component="h2">
+      <Typography variant="h4" component="h1" align="center">
         Formulário de Cadastro
       </Typography>
       <FormRegister />
