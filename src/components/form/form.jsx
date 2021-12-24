acef0e5690f01f3bc217bb4b76de981ea9d2e5fb
celio <celio.ldn@gmail.com>
2021-12-24 11:49:29 -0300

    Ajusted useState

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,25 +1,39 @@
-import React from "react";
+import { React, useState } from "react";
 import { Button, TextField, Switch, FormControlLabel } from "@material-ui/core";
 
 function FormRegister() {
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+
   return (
-    <form>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        console.log(name, surname);
+      }}
+    >
       <TextField
+        value={name}
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
         variant="outlined"
         id="name"
         label="Nome"
         fullWidth
         margin="dense"
       />
-
       <TextField
+        value={surname}
+        onChange={(event) => {
+          setSurname(event.target.value);
+        }}
         variant="outlined"
         id="surname"
         label="Sobrenome"
         fullWidth
         margin="dense"
       />
-
       <TextField
         variant="outlined"
         id="CPF"
@@ -27,17 +41,14 @@ function FormRegister() {
         fullWidth
         margin="dense"
       />
-
       <FormControlLabel
         label="Promoções"
         control={<Switch name="promocoes" color="primary" defaultChecked />}
       />
-
       <FormControlLabel
         label="Newsletter"
         control={<Switch name="newsletter" color="primary" defaultChecked />}
       />
-
       <Button variant="contained" color="primary" type="submit">
         Cadastrar
       </Button>
